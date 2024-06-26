5df5e4b5aac486cbc05019d8e1a841f1667f0784
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-06-26 07:47:01 +0530

    Ensuring gstin validation of the customer model using the validate method and UniqueValidator

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,18 +1,31 @@
-from .models import Customer
+from customers.models import Customer
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
 
-    def to_gstin_representation(self, instance):
-        representation = super().to_gstin_representation(instance)
-        if instance.gst_treatment != Customer.GstTreatment.REGISTERED:
-            representation.pop('gstin', None)
-        return representation
+    GST_TREATMENT_CHOICES = (  
+        ("Unregistered", "UNREGISTERED"),  
+        ("Registered", "REGISTERED"),
+    )
     
+    gst_treatment = serializers.ChoiceField(GST_TREATMENT_CHOICES)
+    gstin = serializers.CharField(max_length = 15, allow_blank=True, validators=[
+        UniqueValidator(queryset=Customer.objects.all(), message='Provided gstin already exists')
+    ])
+
     def validate(self, data):
-        if data.get('gst_treatment') == Customer.GstTreatment.REGISTERED and not data.get('gstin'):
-            raise serializers.ValidationError('GSTIN is required if GST Treatment is registered.')
+        if data['gst_treatment'] == 'Registered' and not data['gstin']:
+            raise serializers.ValidationError({'gstin': 'for Registered entities, gstin is mandatory'})
+
+        if data['gst_treatment'] == 'Unregistered' and data['gstin']:
+            raise serializers.ValidationError({'gstin': 'for Unregistered entities, gstin is not allowed'})
         return data
+    
+        
+    
