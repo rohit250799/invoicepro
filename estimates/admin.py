6f9b8bb0d2a4adf4764cbee1af3815665d402b4b
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-06-28 00:16:04 +0530

    Making sure the estimate_number is auto-generated and not editable in the django admin.

--- estimates/admin.py
+++ estimates/admin.py
@@ -1,3 +1,21 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
+from estimates.models import Estimates
 
 # Register your models here.
+
+class EstimateAdmin(admin.ModelAdmin):
+    readonly_fields = ('estimate_number',)
+
+    def get_readonly_fields(self, request: HttpRequest, obj: Any | None = ...) -> list[str] | tuple[Any, ...]:
+        if obj:
+            return self.readonly_fields + ('other_fields',) #add other fields to make it readonly
+        return self.readonly_fields
+    
+    def save_model(self, request: Any, obj: Any, form: Any, change: Any) -> None:
+        if not obj.estimate_number:
+            obj.save()
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Estimates, EstimateAdmin)
