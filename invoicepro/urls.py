0afc80638426b847ef1f1add1771f9ec37694ca9
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-06-30 10:51:14 +0530

    Adding urlpaths for estimates and items

--- invoicepro/urls.py
+++ invoicepro/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customers/', include('customers.urls')),
+    path('estimates/', include('estimates.urls')),
+    path('items/', include('items.urls')),
 ]
