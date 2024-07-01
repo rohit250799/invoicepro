c370ca5d679a7a0a8fa48dd3db3443a845f4aab0
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-01 23:10:09 +0530

    Creating a defaultrouter instance and registering router paths

--- items/urls.py
+++ items/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from items.views import ItemViewSet, EstimateViewSet
+#from items.views import ItemListCreateView, ItemRetrieveUpdateDeleteView
+
+router = DefaultRouter()
+router.register(r'', ItemViewSet, 'item-list')
+router.register(r'', EstimateViewSet, 'estimate-list')
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
