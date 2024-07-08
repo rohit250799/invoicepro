57444f7a51fc0aee645943cfd69b857deaad4eb2
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-08 08:46:16 +0530

    Implementing connection pooling - max number of connections allowed = 20 and for the last commit message mistake - ensuring estimate_date <= offer_expiry_date

--- invoicepro/settings.py
+++ invoicepro/settings.py
@@ -95,6 +95,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'MAX_CONNS': 5,
+        }
     }
 }
 
