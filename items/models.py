edc3d35ce5934830bb71d377f1b8baf151300935
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-12 07:12:06 +0530

    Adding inventory threshold quantity field in Item model

--- items/models.py
+++ items/models.py
@@ -28,6 +28,7 @@ class Item(models.Model):
     taxes_applicable = models.BooleanField(default=False)
     tax_percentage_on_item = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     available_stock = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True)
+    inventory_threshold_quantity = models.DecimalField(max_digits=10, decimal_places=2, default=1.00)
 
     def __str__(self):
         return self.name
