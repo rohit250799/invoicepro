c65aedfda92e3d44a19f2c41e27f79973d0c64dc
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-19 20:12:09 +0530

    Add: urlpatterns for users app

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from users.views import UserCreateView, UserUpdateView
 
 urlpatterns = [
-    
+    path('create_user/', UserCreateView.as_view(), name='user-create'),
+    path('update_user/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
 ]
