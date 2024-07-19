dc0b5ed0cc7105be25e002cbf6e3346c0a18f0c1
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-19 20:14:05 +0530

    Add: UserAdmin model to register and latest estimate migration file

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from users.models import UserProfileInfo
 
 # Register your models here.
 
-admin.site.register(UserProfileInfo)
+admin.site.register(UserProfileInfo, UserAdmin)
