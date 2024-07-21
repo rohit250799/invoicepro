918beda6887ac1f9484211023221d93904fd3eed
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-21 19:00:14 +0530

    add: url patterns for register, login and logout views

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from users.views import UserCreateView, UserUpdateView
+from users.views import UserCreateView, UserUpdateView, register_user, user_login, user_logout
+
 
 urlpatterns = [
     path('create_user/', UserCreateView.as_view(), name='user-create'),
     path('update_user/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
+    path('register/', register_user, name='register_user'),
+    path('login/', user_login, name='user_login'),
+    path('logout/', user_logout, name='user_logout'),
 ]
