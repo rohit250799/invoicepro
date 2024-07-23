9a7121b8880c1c0529bb17afe50643f51beb18f8
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-23 22:50:35 +0530

    Disabling all password hashers except Argon2

--- invoicepro/settings.py
+++ invoicepro/settings.py
@@ -112,10 +112,10 @@ REST_FRAMEWORK = {
 #for Password Hashing:
 PASSWORD_HASHERS = {
     "django.contrib.auth.hashers.Argon2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
-    "django.contrib.auth.hashers.ScryptPasswordHasher",
+    # "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    # "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    # "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    # "django.contrib.auth.hashers.ScryptPasswordHasher",
 }
 
 #for authentication backends:
