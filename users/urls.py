dc72a68525dfcfdae1f68341b8e98a41b3ee1b49
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-24 08:17:21 +0530

    add: static paths to urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from users.views import UserCreateView, UserUpdateView, register_user, user_login, user_logout
+from django.conf.urls.static import static
+from django.conf import settings
+from users.views import UserCreateView, UserUpdateView, register_user, user_login, user_logout, siteIndex
 
 
 urlpatterns = [
+    path('', siteIndex, name='invoicepro index'),
     path('create_user/', UserCreateView.as_view(), name='user-create'),
     path('update_user/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
     path('register/', register_user, name='register_user'),
     path('login/', user_login, name='user_login'),
     path('logout/', user_logout, name='user_logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
