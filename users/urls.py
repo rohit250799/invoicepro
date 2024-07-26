6a1b29feacf97bfbcea6ed8bc8d471571f1e6af1
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-26 21:28:36 +0530

    adding: url paths for view and logout user removed: paths for register, login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from users.views import  RegisterView, LoginView, register_user, user_login, user_logout, siteIndex
+from users.views import  RegisterView, LoginView, UserView, LogoutView
 
 
 urlpatterns = [
-    path('', siteIndex, name='invoicepro index'),
+    #path('', siteIndex, name='invoicepro index'),
     # path('create_user/', UserCreateView.as_view(), name='user-create'),
     # path('update_user/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
     path('register_user/', RegisterView.as_view(), name='register-view'),
     path('login_user/', LoginView.as_view(), name='login-view'),
-    path('register/', register_user, name='register_user'),
-    path('login/', user_login, name='user_login'),
-    path('logout/', user_logout, name='user_logout'),
+    path('view_user/', UserView.as_view(), name='user-view'),
+    path('logout_user/', LogoutView.as_view(), name='user-logout'),
+    # path('register/', register_user, name='register_user'),
+    # path('login/', user_login, name='user_login'),
+    # path('logout/', user_logout, name='user_logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
