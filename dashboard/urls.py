8f560d6ca3e2df64f1144717e4732b6d96548606
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-28 18:27:06 +0530

    adding: login redirection urlpattern

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from dashboard.views import siteIndex
+from dashboard.views import siteIndex, login_redirection
 
 urlpatterns = [
     path('', siteIndex, name='invoicepro index'),
+    path('user_dashboard/', login_redirection, name='login-redirection'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
