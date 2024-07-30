316dd4817d85ef0569c93bcdcba45fa8b51bb509
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-30 23:36:19 +0530

    adding: estimate creation view by rendering serializer as form

--- estimates/views.py
+++ estimates/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import generics, viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
 from estimates.models import Estimates, EstimateItems
-from .serializers import EstimateSerializer
+from estimates.serializers import EstimateSerializer
+from estimates.forms import EstimateForm
 
+from django.template.response import SimpleTemplateResponse
 import io
 from django.http import FileResponse
 from reportlab.pdfgen import canvas
@@ -12,6 +17,33 @@ from reportlab.pdfgen import canvas
 class EstimateListCreateView(generics.ListCreateAPIView):
     queryset = Estimates.objects.all()
     serializer_class = EstimateSerializer
+
+class EstimateCreationView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'estimates/estimate_form.html'
+
+    def get(self, request, id):
+        # form = EstimateForm()
+        # return render(request, 'estimates/estimate_form.html', {'form': form})
+        estimate = get_object_or_404(Estimates, id=id)
+        serializer = EstimateSerializer(estimate)
+        return Response({'serializer': serializer, 'estimate': estimate})
+        
+    
+    def post(self, request, id):
+        # form = EstimateForm(request.POST)
+        # if form.is_valid():
+        #     estimate = form.save()
+        #     serializer = EstimateSerializer(estimate)
+        #     return redirect('estimate-pdf-creation')
+        # return render(request, 'estimate_form.html', {'form': form})
+
+        estimate = get_object_or_404(Estimates, pk=id)
+        serializer = EstimateSerializer(estimate, data=request.data)
+        if not serializer.is_valid():
+            return Response({'serializer': serializer, 'estimate': estimate})
+        serializer.save()
+        return redirect('estimate-list-create')
 
 class EstimateRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Estimates.objects.all()
