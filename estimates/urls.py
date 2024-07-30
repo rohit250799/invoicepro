d21ffeda382126391a38730baa035a4894049cd9
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-30 08:51:37 +0530

    adding: url pattern for estimate pdf creation view

--- estimates/urls.py
+++ estimates/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView
+from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView, estimate_pdf_creation_view
 
 
 urlpatterns = [
     path('', EstimateListCreateView.as_view(), name='estimate-list-create'),
     path('<int:pk>/', EstimateRetrieveUpdateDeleteView.as_view(), name='estimate-retrieve-update-delete'),
+    path('pdf/', estimate_pdf_creation_view, name='estimate-pdf-creation'),
 ]
