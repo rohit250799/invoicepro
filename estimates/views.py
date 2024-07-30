bdb047c471935014a7bbab7550bd2c963bba2137
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-07-30 08:49:33 +0530

    adding: estimate pdf creation view

--- estimates/views.py
+++ estimates/views.py
@@ -3,6 +3,10 @@ from rest_framework import generics, viewsets
 from estimates.models import Estimates, EstimateItems
 from .serializers import EstimateSerializer
 
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+
 # Create your views here.
 
 class EstimateListCreateView(generics.ListCreateAPIView):
@@ -16,3 +20,12 @@ class EstimateRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
 class EstimateViewSet(viewsets.ModelViewSet):
     queryset = Estimates.objects.all()
     serializer_class = EstimateSerializer
+
+def estimate_pdf_creation_view(request):
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+    p.drawString(100, 100, 'Testing pdf creation')
+    p.showPage()
+    p.save()
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='estimate.pdf')
