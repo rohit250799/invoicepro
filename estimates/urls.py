5ca696a0c305d1d4852eef6cf9eaf688bc6f0a9a
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-08-02 09:00:27 +0530

    adding: estimate detail url pattern

--- estimates/urls.py
+++ estimates/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView, estimate_pdf_creation_view, EstimateCreationView
+from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView, estimate_pdf_creation_view, EstimateCreationView, EstimateDetailView
 
 
 urlpatterns = [
     path('', EstimateListCreateView.as_view(), name='estimate-list-create'),
     path('<int:pk>/', EstimateRetrieveUpdateDeleteView.as_view(), name='estimate-retrieve-update-delete'),
     path('pdf/', estimate_pdf_creation_view, name='estimate-pdf-creation'),
-    path('create_estimate/<int:id>/', EstimateCreationView.as_view(), name='create-estimate'),
+    path('create_estimate/', EstimateCreationView.as_view(), name='create-estimate'),
+    path('detail_estimate/<int:estimate_id>/', EstimateDetailView.as_view(), name='estimate-detail'),
 ]
