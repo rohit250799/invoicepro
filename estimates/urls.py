deb2c124ea539fb9f5aa5a3ea59390bd67e2464b
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-08-06 08:42:27 +0530

    adding: estimates list and its url path for template testing

--- estimates/urls.py
+++ estimates/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView, estimate_pdf_creation_view, EstimateCreationView, EstimateDetailView
+from estimates.views import EstimateRetrieveUpdateDeleteView, estimate_pdf_creation_view, EstimateCreationView, EstimateDetailView, estimates_index
 
 
 urlpatterns = [
-    path('', EstimateListCreateView.as_view(), name='estimate-list-create'),
+    #path('', EstimateListCreateView.as_view(), name='estimate-list-create'),
     path('<int:pk>/', EstimateRetrieveUpdateDeleteView.as_view(), name='estimate-retrieve-update-delete'),
     path('pdf/', estimate_pdf_creation_view, name='estimate-pdf-creation'),
     path('create_estimate/', EstimateCreationView.as_view(), name='create-estimate'),
     path('detail_estimate/<int:estimate_id>/', EstimateDetailView.as_view(), name='estimate-detail'),
+    path('index/', estimates_index, name='estimates-index'),
 ]
