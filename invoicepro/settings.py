247473decd75c7d1e9453c86c89b0963f415bea8
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-08-08 22:20:57 +0530

    Revert "adding: email backend"
    This reverts commit c72e0ac847e025a119cad59859d4e4be6acbadd8.

--- invoicepro/settings.py
+++ invoicepro/settings.py
@@ -262,9 +262,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#Email configurations
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
