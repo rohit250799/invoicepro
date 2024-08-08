dd384dd06aeeef18ef2aaec85e85b3f63e383079
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-08-08 23:16:52 +0530

    modifying: estimate_form and estimate urls

--- estimates/urls.py
+++ estimates/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from estimates.views import EstimateRetrieveUpdateDeleteView, estimate_pdf_creation_view, EstimateCreationView, EstimateDetailView, estimates_index
+from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView, estimate_pdf_creation_view, EstimateCreationView, EstimateDetailView, estimates_index
 
 
 urlpatterns = [
-    #path('', EstimateListCreateView.as_view(), name='estimate-list-create'),
+    path('index/', EstimateListCreateView.as_view(), name='estimate-list-create'),
     path('<int:pk>/', EstimateRetrieveUpdateDeleteView.as_view(), name='estimate-retrieve-update-delete'),
     path('pdf/', estimate_pdf_creation_view, name='estimate-pdf-creation'),
     path('create_estimate/', EstimateCreationView.as_view(), name='create-estimate'),
     path('detail_estimate/<int:estimate_id>/', EstimateDetailView.as_view(), name='estimate-detail'),
-    path('index/', estimates_index, name='estimates-index'),
+    #path('index/', estimates_index, name='estimates-index'),
 ]
