48a1abe7295fc00797fa3ceb55a76a7c59a864f8
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-08-09 08:26:38 +0530

    renaming: messages to status_message and removing unnecessary code

--- users/views.py
+++ users/views.py
@@ -22,64 +22,6 @@ import datetime, pytz, os, jwt
 User = get_user_model()
 secret_key = os.getenv('SECRET_KEY')
 
-# @api_view(['POST'])
-# def register_user(request):
-#     if request.method == 'POST':
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             #request.status_code = 201
-#             messages.success(request, 'User created succesfully!')
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['POST'])
-# def user_login(request):
-#     if request.method == 'POST':
-#         username = request.data.get('username')
-#         password = request.data.get('password')
-
-#         user = None
-#         if '@' in username:
-#             try: 
-#                 user = UserProfileInfo.objects.get(email=username)
-#             except ObjectDoesNotExist: pass
-        
-#         if not user: user = authenticate(username=username, password=password)
-        
-#         if user:
-#             token, _ = Token.objects.get_or_create(user=user)
-#             messages.success(request, 'User logged in')
-#             return Response({'token': token.key}, status=status.HTTP_200_OK)
-        
-#         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
-# @api_view(['POST'])
-# @permission_classes([IsAuthenticated])    
-# def user_logout(request):
-#     if request.method == 'POST':
-#         try: 
-#             request.user.auth_token.delete()
-#             return Response({'message': 'Succesfully logged out'}, status=status.HTTP_200_OK)
-#         except Exception as e:
-#             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-# class UserCreateView(generics.CreateAPIView):
-#     model = get_user_model()
-#     permission_classes = [
-#         permissions.AllowAny
-#     ]
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
-# class UserUpdateView(generics.RetrieveUpdateDestroyAPIView):
-#     model = get_user_model()
-#     permission_classes = [
-#         permissions.AllowAny
-#     ]
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -131,7 +73,7 @@ class LoginView(APIView):
         response.data = {
             'jwt': encoded_token
         }
-        messages.success(request, 'Login succesful')
+        status_message.success(request, 'Login succesful')
         #return response
         return redirect("/user_dashboard/", response)
 
