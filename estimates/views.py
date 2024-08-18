4ebca6cc8e586a55247b395f467b542658fa42a0
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-08-18 11:46:00 +0530

    adding: pdf generation view class for testing

--- estimates/views.py
+++ estimates/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.utils.html import strip_tags
+from django.http import HttpResponse
 from rest_framework import generics, viewsets, status, serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response 
+from reportlab.lib.pagesizes import letter
 from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer, BrowsableAPIRenderer
 from estimates.models import Estimates, EstimateItems
 from estimates.serializers import EstimateSerializer
@@ -10,6 +14,7 @@ from estimates.forms import EstimateForm
 #from django.template.response import SimpleTemplateResponse
 import io
 from django.http import FileResponse
+from django.template.loader import render_to_string
 from reportlab.pdfgen import canvas
 
 # Create your views here.
@@ -59,6 +64,27 @@ class EstimateRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
 class EstimateViewSet(viewsets.ModelViewSet):
     queryset = Estimates.objects.all()
     serializer_class = EstimateSerializer
+
+class PdfGenerationView(View):
+    def get(self, request, estimate_id, *args, **kwargs):
+        response = HttpResponse(content_type='application/pdf')
+        estimate = Estimates.objects.get(pk=estimate_id)
+        response['Content-Disposition'] = 'attachment; filename="estimate.pdf"'
+        html_string = render_to_string('estimates/estimate_pdf_template.html', {
+            'estimate_number': estimate.estimate_number,  
+            # 'customer': 11,
+            # 'estimate_date': '2024-08-12',
+            # 'offer_expiry_date': '2024-08-19',
+            'title': 'Test title',
+            'content': 'Test content'
+        })
+        text_content = strip_tags(html_string)
+
+        p = canvas.Canvas(response, pagesize=letter)
+        p.drawString(30, 350, text_content)
+        p.showPage()
+        p.save()
+        return response
 
 def estimate_pdf_creation_view(request):
     buffer = io.BytesIO()
