fa7eadfb7826eea6062c9de9610ae8b168d8dd95
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-08-18 12:11:14 +0530

    adding: pdf generation view url for testing

--- estimates/urls.py
+++ estimates/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView, estimate_pdf_creation_view, EstimateCreationView, EstimateDetailView, estimates_index
+from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView, estimate_pdf_creation_view, EstimateCreationView, EstimateDetailView, estimates_index, PdfGenerationView
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('pdf/', estimate_pdf_creation_view, name='estimate-pdf-creation'),
     path('create_estimate/', EstimateCreationView.as_view(), name='create-estimate'),
     path('detail_estimate/<int:estimate_id>/', EstimateDetailView.as_view(), name='estimate-detail'),
+    path('generate_pdf/<int:estimate_id>/', PdfGenerationView.as_view(), name='generate-pdf'),
     #path('index/', estimates_index, name='estimates-index'),
 ]
