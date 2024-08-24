54c5ab48c5bc9776b46fd8c66708b5b83ce95bac
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-08-24 22:02:34 +0530

    editing: sending estimate url path

--- estimates/urls.py
+++ estimates/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView, EstimateCreationView, EstimateDetailView, estimates_index, PdfGenerationView, ViewPDF
+from estimates.views import EstimateListCreateView, EstimateRetrieveUpdateDeleteView, EstimateCreationView, EstimateDetailView, estimates_index, PdfGenerationView, ViewPDF, send_estimate_to_customer
 
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
     path('detail_estimate/<int:estimate_id>/', EstimateDetailView.as_view(), name='estimate-detail'),
     path('generate_pdf/<int:estimate_id>/', PdfGenerationView.as_view(), name='generate-pdf'),
     path('pdf_view/<int:estimate_id>/', ViewPDF.as_view(), name='pdf-view'),
+    path('send_estimate/<int:estimate_id>/', send_estimate_to_customer, name='send-estimate'),
     #path('index/', estimates_index, name='estimates-index'),
 ]
