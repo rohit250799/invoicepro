92f5a00f6b06e9ae01403540a03143497f0c6419
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-09-24 21:06:27 +0530

    changing: create method

--- estimates/serializers.py
+++ estimates/serializers.py
@@ -6,17 +6,14 @@ from items.models import Item
 from customers.models import Customer
 
 class EstimateItemSerializer(serializers.ModelSerializer):
-    #product = ItemSerializer()
 
     class Meta:
         model = EstimateItems
-        #fields = ['quoteitems_id', 'product', 'offered_quantity_to_customer', 'selling_price_proposed_to_customer']
-        fields = ['name', 'offered_quantity_to_customer', 'selling_price_proposed_to_customer']
+        fields = ['name', 'offered_quantity_to_customer']
 
 
 class EstimateSerializer(serializers.ModelSerializer):
     items = EstimateItemSerializer(many=True)
-    #offered_estimate_items = EstimateItemSerializer(many=True)
 
     
     class Meta:
@@ -24,39 +21,15 @@ class EstimateSerializer(serializers.ModelSerializer):
         fields = ['estimate_number', 'customer', 'estimate_date', 'offer_expiry_date', 'items']
 
     def create(self, validated_data):
-        items_data = validated_data.pop('items')
+        items_data = validated_data.pop('items', [])
         estimate = Estimates.objects.create(**validated_data)
-        #items_data = estimate.items.all()
-        # Handle each item in the items_data
-        #item_ids = [item_data['quoteitems_id'] for item_data in items_data]  # Extract IDs from the item data
         for item_data in items_data:
-            #item_id = EstimateItems.objects.get(product=item_data['product'].id) - testing with below line
-            item = Item.objects.get(id=item_data['name'].id)
-            #item = Item.objects.create(estimate=estimate, **item_data)
-            # item, created = Item.objects.get_or_create(
-            #     name=item_data['product'],
-            #     defaults={
-            #         'price': item_data.get('selling_price_proposed_to_customer', 1.0),
-            #     }
-            # ) - testing (latest)
+            item_id = item_data['name'].pk
+            item = Item.objects.get(id=item_id)
+
             offered_quantity_to_customer = item_data.get('offered_quantity_to_customer', 1)
-            #offered_quantity_to_customer = item_data['offered_quantity_to_customer']
 
-            #selling_price_proposed_to_customer = item_data.get('selling_price_proposed_to_customer', 1)
-            #selling_price_proposed_to_customer = item_data['selling_price_proposed_to_customer']
-
-
-            #estimate.offered_estimate_items.add(item) - test
-            #EstimateItems.objects.create(estimate=estimate, **item_data)
-        # estimate.items.set(item_ids)
-
-        #estimate.items.set(items_data) #testing
-
-        #creating EstimateItems object with imp fields
-            EstimateItems.objects.create(
-                estimate=estimate, name     =item, offered_quantity_to_customer=offered_quantity_to_customer,
-                selling_price_proposed_to_customer=item_data.get('selling_price_proposed_to_customer', 1.0)
-            )
+            EstimateItems.objects.create(estimate=estimate, name=item, offered_quantity_to_customer=offered_quantity_to_customer)
 
         return estimate
 
